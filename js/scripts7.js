85c847eac1b5a9d927d02fa703dfec1156c25684
Agnes NM <agnesndutamwaniki@gmail.com>
2019-03-04 03:00:52 +0300

    test UI logic to make the dice roll and generate random numbers, test more bootstrap elements

--- js/scripts7.js
+++ js/scripts7.js
@@ -26,32 +26,32 @@ $(document).ready(function(){
 
   $("h1#player1").append(newPlayer.prototype.name);
 
-  $("button#p1info").last().click(function(){
-  $("h1#player1").text(newPlayer.prototype.name());
-  $("p#p1FN").text(newPlayer.firstName);
-  $("p#p1LN").text(newPlayer.lastName);
+  $("button#p1Info").last().click(function(){
+    $("h1#player1").text(newPlayer.prototype.name());
+    $("p#p1FN").text(newPlayer.firstName);
+    $("p#p1LN").text(newPlayer.lastName);
   });
-  $("input#p1FName").val("");
-  $("input#p1LName").val("");
+    $("input#p1FName").val("");
+    $("input#p1LName").val("");
   });
   $("form#p2Details").submit(function(event){
-	event.PreventDefault();
-  var p2FirstNameInput=$("input#p2FName").val();
-  var p2LastNameInput=$("input#p2LName").val();
-  var Player2=new Player(p2FirstNameInput,p2LastNameInput);
+	   event.PreventDefault();
+     var p2FirstNameInput=$("input#p2FName").val();
+     var p2LastNameInput=$("input#p2LName").val();
+     var Player2=new Player(p2FirstNameInput,p2LastNameInput);
 
   $("h2#player2").append(newPlayer.prototype.name);
 
   $("button#p2Info").last().click(function(){
-  $("h2#player2").text(newPlayer.name());
-  $("p#p2FN").text(newPlayer.firstName);
-  $("p#p2LN").text(newPlayer.lastName);
-  });
-  $("input#p2FName").val("");
-  $("input#p2LName").val("");
+    $("h2#player2").text(newPlayer.name());
+    $("p#p2FN").text(newPlayer.firstName);
+    $("p#p2LN").text(newPlayer.lastName);
+    });
+    $("input#p2FName").val("");
+    $("input#p2LName").val("");
   });
   $("button#p1roll").click(function(){
-  function player1() {
+    function player1() {
     document.getElementbyId("p1roll").disabled = false;
     document.getElementbyId("p2roll").disabled = true;
   }
@@ -59,34 +59,34 @@ $(document).ready(function(){
     document.getElementbyId("p1roll").disabled = true;
     document.getElementbyId("p2roll").disabled = false;
   }
-  var rollScore =0;
-  var totalScore=0;
-  var turn = Math.floor((Math.random()*6)+1);
+    var rollScore =0;
+    var totalScore=0;
+    var turn = Math.floor((Math.random()*6)+1);
   $("p#p1Roll").text("Your score is " + Player.prototype.roll);
     if (turn !== 1) {
     rollScore+=turn;
-  }else{
+    }else{
     rollScore=0;
     player2();
   }
   $("p#p1RollScore").text("Your score is " + Player.prototype.roll);
   });
-  $("button#p1hold").click(function(){
-    totalScore+=rollScore;
-    alert(rollScore);
-    rollScore=0;
-    if(totalrollScore===100){
+    $("button#p1hold").click(function(){
+      totalScore+=rollScore;
+      alert(rollScore);
+      rollScore=0;
+      if(totalrollScore===100){
       alert("You are the winner!")
-    }else{
+      }else{
       player2();
-    }
-    var rollScore =0;
-    var totalScore=0;
-    var turn = Math.floor((Math.random()*6)+1);
+      }
+      var rollScore =0;
+      var totalScore=0;
+      var turn = Math.floor((Math.random()*6)+1);
     $("p#p2Roll").text("Your score is " + Player.prototype.roll);
       if (turn !== 1) {
       rollScore+=turn;
-    }else{
+      }else{
       rollScore=0;
       player1();
     }
--- js/scripts8.js
+++ js/scripts8.js
@@ -1,14 +1,14 @@
 //Business Logic
 //Player Object constructor
 function Player(name) {
-  this.firstName = first;
-  this.lastName = last;
-  this.score = [];
+  // this.firstName = first;
+  // this.lastName = last;
+  // this.score = [];
 }
 //Methods attached to Player
-Player.prototype.name = function () {
-  return this.firstName + " " + this.lastName;
-}
+// Player.prototype.name = function () {
+//   return this.firstName + " " + this.lastName;
+// }
 Player.prototype.roll = function(){
   return Math.floor((Math.random()*6)+1);
 }
@@ -19,33 +19,39 @@ Player.prototype.hold = function (){
 //UI Logic
 $(document).ready(function(){
 	$("form#p1Details").submit(function(event){
-  event.PreventDefault();
-  var p1FirstNameInput=$("#p1FName").val();
-  var p1LastNameInput=$("#p1LName").val();
-  var player1=new Player(p1FirstNameInput,p1LastNameInput);
-  $("#p1FName").text(" "  + Player.firstName);
-  $("#p1LName").text(" "  + Player.lastName);
-  $("#player1").text(Player.prototype.name);
+	event.PreventDefault();
+  var p1FirstNameInput=$("input#p1FName").val();
+  var p1LastNameInput=$("input#p1LName").val();
+  var Player1=new Player(p1FirstNameInput,p1LastNameInput);
 
+  $("h1#player1").append(newPlayer.prototype.name);
 
-  //$("input#p1FName").val("");
-  //$("input#p1LName").val("");
+  $("button#p1Info").last().click(function(){
+    $("h1#player1").text(newPlayer.prototype.name());
+    $("p#p1FN").text(newPlayer.firstName);
+    $("p#p1LN").text(newPlayer.lastName);
+  });
+    $("input#p1FName").val("");
+    $("input#p1LName").val("");
   });
   $("form#p2Details").submit(function(event){
-	event.PreventDefault();
-  var p2FirstNameInput=$("input#p2FName").val();
-  var p2LastNameInput=$("input#p2LName").val();
-  var Player2=new Player(p2FirstNameInput,p2LastNameInput);
-
+	   event.PreventDefault();
+     var p2FirstNameInput=$("input#p2FName").val();
+     var p2LastNameInput=$("input#p2LName").val();
+     var Player2=new Player(p2FirstNameInput,p2LastNameInput);
 
   $("h2#player2").append(newPlayer.prototype.name);
 
-
-  $("input#p2FName").val("");
-  $("input#p2LName").val("");
+  $("button#p2Info").last().click(function(){
+    $("h2#player2").text(newPlayer.name());
+    $("p#p2FN").text(newPlayer.firstName);
+    $("p#p2LN").text(newPlayer.lastName);
+    });
+    $("input#p2FName").val("");
+    $("input#p2LName").val("");
   });
   $("button#p1roll").click(function(){
-  function player1() {
+    function player1() {
     document.getElementbyId("p1roll").disabled = false;
     document.getElementbyId("p2roll").disabled = true;
   }
@@ -53,48 +59,50 @@ $(document).ready(function(){
     document.getElementbyId("p1roll").disabled = true;
     document.getElementbyId("p2roll").disabled = false;
   }
-  var rollScore =0;
-  var totalScore=0;
-  var turn = Math.floor((Math.random()*6)+1);
-  $("p#p1Roll").text("Your score is " + Player.prototype.roll);
-    if (turn !== 1) {
-    rollScore+=turn;
-  }else{
-    rollScore=0;
-    player2();
-  }
-  $("p#p1RollScore").text("Your score is " + Player.prototype.roll);
-  });
-  $("button#p1hold").click(function(){
-    totalScore+=rollScore;
-    alert(rollScore);
-    rollScore=0;
-    if(totalrollScore===100){
-      alert("You are the winner!")
-    }else{
-      player2();
-    }
     var rollScore =0;
     var totalScore=0;
-    var turn = Math.floor((Math.random()*6)+1);
-    $("p#p2Roll").text("Your score is " + Player.prototype.roll);
+    var turn = Player.prototype.roll;
       if (turn !== 1) {
       rollScore+=turn;
-    }else{
-      rollScore=0;
-      player1();
+      }else{
+      rollScore=turn;
+      player2();
     }
-    $("p#p2RollScore").text("Your score is " + Player.prototype.roll);
-    });
-    $("button#p2hold").click(function(){
-      totalScore+=rollScore;
+    totalScore += (Math.floor((Math.random()*6)+1));
+  $("p#p1Roll").text("Your score is " + turn);
+  $("p#p1RollScore").text("Your total score is " + totalScore);
+  });
+    $("button#p1hold").click(function(){
+      var holdScore = Player.prototype.hold
       alert(rollScore);
       rollScore=0;
-      if(totalrollScore===100){
-        alert("You are the winner!")
+      if(totalScore===100){
+      alert("You are the winner!")
       }else{
+      player2();
+      }
+    var rollScore =0;
+    var totalScore=0;
+    var turn = Player.prototype.roll;
+      if (turn !== 1) {
+        rollScore+=turn;
+      }else{
+        rollScore=turn;
         player1();
       }
+      totalScore += (Math.floor((Math.random()*6)+1));
+    $("p#p2Roll").text("Your score is " + turn);
+    $("p#p2RollScore").text("Your total score is " + totalScore);
+    });
+      $("button#p2hold").click(function(){
+        totalScore+=rollScore;
+        alert(rollScore);
+        rollScore=0;
+        if(totalScore===100){
+        alert("You are the winner!")
+        }else{
+        player1();
+        }
   });
 });
 //clean
